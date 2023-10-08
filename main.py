90fb6316ffeceb53ef14ad7b359b6f20cf12b1e4
leracereusenco <valeria.cereusenco@gmail.com>
2023-10-08 10:59:46 +0300

    main

---
+++ main.py
@@ -0,0 +1,12 @@
+def functionName(parametri, parametru2):
+    print(parametru2)
+    print(parametri*3)
+
+var = '4'
+
+if var == 4:
+    functionName(var, parametru2= 'Number: ')
+elif var == '4':
+    functionName(var, parametru2= 'String: ')
+else:
+    print('smth else')
